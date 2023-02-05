0a9596c59c886a9407d7cbdd9052ba9aa72bba9a
enish0 <yasin.enis0891@gmail.com>
2023-02-05 16:22:22 +0300

    en son hal

--- 5 ] Koşul İfadeleri/4) Koşul idadeler Test2.py
+++ 5 ] Koşul İfadeleri/4) Koşul idadeler Test2.py
@@ -1,1 +1,27 @@
-deneme = '1234'
+# 1- Girilen sayının 0-100 arasında olup olmadığını kontrol edin
+sayi = int(input('Bir sayı gir'))
+
+
+
+
+
+
+# 2- Girilen bir sayının pzitif .ift sayı olup olmadığını kontrol edin
+
+# 3- Email ve parola bilgileri ile giriş kontrolü yapınız
+#         email : email@sadikturan.com
+#         password : abc123
+
+# 4- Girilen 3 sayıyı büyüklük olarak karşılaştırınız
+
+# 5- Kullanıcıdan 2 vize (%60) ve final (%40) notunu alıp ortalama hesaplayınız.
+#         Eğer ortalama 50 ve üstündeyse geçti değilse kaldı yazdırın
+#          a-) Ortalama 50 olsa bile final notu en az 50 olmalıdır
+#          b-) Finalden 70 alındığında ortalamanın önemi olmasın
+
+# 6- Kişinin ad , kilo ve boy bilgilerini alıp kilo endekslerini hesaplayınız.
+#         Formül: (kilo / boy uzunluğunun karesi)
+#         0-18.4  =>   Zayıf 
+#         18.5-24.9  =>   Normal 
+#         25.0-29.9  =>   Fazla Kilolu 
+#         30.0-34.9  =>   Şişman(Obez)
