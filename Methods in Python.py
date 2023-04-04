af7b616f41d3472d0e86396cf8c6425da4eb5c30
enish0 <yasin.enis0891@gmail.com>
2023-04-04 15:55:02 +0300

    method tanımı

---
+++ Methods in Python.py
@@ -0,0 +1,15 @@
+# method nedir , hangi amaçla kullanılır.
+
+list = [1,2,3]
+list.append(4)
+# terminal : listeye 4 ekler . bu append (metodudur).
+print(type(list))
+# terminal : <class> 'list' der 
+
+# metodların birleşik hali de class tır.
+
+myString = 'Hello'
+print(type(myString)) # class tipini yazar cevap string
+
+
+# sonuç: Class lar içine tanımlanmış belirli metodlar vardır
